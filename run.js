a43c609d3f9a570bebf9933d02dfeb3e709797e7
josfredus <59462599+josfredus@users.noreply.github.com>
2020-01-05 01:41:52 +0100

    Add files via upload

--- run.js
+++ run.js
@@ -1,17 +1,18 @@
 const runTheShow = setup => new Promise((res, rej) => {
   document.body.style.overflow = "hidden";
   document.body.scroll = "no";
+  document.body.style.margin = 0 // remove after testing it's in css already
   const dataDisplay = createDataDisplay();
   const timeDisplay = createTimeDisplay();
   const media = createMedia();
   
   timeDisplay.set(22.1, 30);
   timeDisplay.setNumber(28);
-  timeDisplay.pause();
   timeDisplay.draw();
   const xtr = new ContentExtractor("aww");
   xtr.getNextContent().then(media.set).catch(console.log("BARREN"));
 });
 
-//window.onload = () => setUpTheShow().then(launchSlideshow).catch(console.log);
-window.onload = () => runTheShow({});
+// window.onload = () => setUpTheShow().then(launchSlideshow).catch(console.log);
+// window.onload = () => runTheShow({});
+window.onload = () => setUpTheShow().then(console.log, console.log);
